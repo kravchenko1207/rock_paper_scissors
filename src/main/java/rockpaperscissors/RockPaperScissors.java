2498a4133f0d6292214367b12c85204642191e17
Iryna Kravchenko <kravchenkoiryna1207@gmail.com>
2025-09-17 00:11:04 +0200

    Initial commit: add RockPaperScissors game with GUI

---
+++ src/main/java/rockpaperscissors/RockPaperScissors.java
@@ -0,0 +1,64 @@
+package rockpaperscissors;
+
+import java.util.Random;
+
+public class RockPaperScissors {
+    private static final String[] computerChoices = {"Rock", "Paper", "Scissors"};
+
+    public String getComputerChoice() {
+        return computerChoice;
+    }
+
+    public int getComputerScore() {
+        return computerScore;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    private String computerChoice;
+    private int computerScore, playerScore;
+
+    private Random random;
+
+    public RockPaperScissors() {
+        random = new Random();
+    }
+
+    public String playRockPaperScissor(String playerChoice) {
+        computerChoice = computerChoices[random.nextInt(computerChoices.length)];
+
+        String result;
+        if (computerChoice.equals("Rock")) {
+            if (playerChoice.equals("Paper")) {
+                result = "Player Wins";
+                playerScore++;
+            } else if (playerChoice.equals("Scissors")) {
+                result = "Computer Wins";
+            } else {
+                result = "Draw";
+            }
+            } else if (computerChoice.equals("Paper")) {
+                if (playerChoice.equals("Scissors")) {
+                    result = "Player Wins";
+                    playerScore++;
+                } else if (playerChoice.equals("Rock")) {
+                    result = "Computer Wins";
+                } else {
+                    result = "Draw";
+                }
+            } else {
+                if (playerChoice.equals("Rock")) {
+                    result = "Player Wins";
+                    playerScore++;
+                } else if (playerChoice.equals("Paper")) {
+                    result = "Computer Wins";
+                } else {
+                    result = "Draw";
+                }
+            }
+            return result;
+        }
+    }
+
